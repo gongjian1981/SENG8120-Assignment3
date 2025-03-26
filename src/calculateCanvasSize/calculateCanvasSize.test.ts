db34ca54e517c40927148fe8b2ebbe96a1a60775
Jian Gong <gongjian1981@gmail.com>
2025-03-26 15:10:30 -0400

    add: add test for calculateCanvasSize for 0

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -12,4 +12,12 @@ describe("calculateCanvasSize", () => {
     // Assert
     expect(result).toEqual(1000);
   })
+
+  test('should return 0 for length and width of 0', () => {
+    expect(calculateCanvasSize('0', '1')).toBe(0);
+    expect(calculateCanvasSize('1', '0')).toBe(0);
+    expect(calculateCanvasSize('0', '0')).toBe(0);
+  });
+
+
 });
