1b5b665ed6799324ac359ca923566348f59ec824
Jian Gong <gongjian1981@gmail.com>
2025-03-26 14:56:39 -0400

    fix: edit code in calculateCanvasPerimeter to pass the test for 0

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
@@ -2,5 +2,10 @@ export default function calculateCanvasPerimeter(
   length: string,
   width: string
 ) {
+  const l = parseInt(length);
+  const w = parseInt(width);
+  if (l == 0 || w == 0) {
+    return 0;
+  }
   return 2 * (parseInt(length) + parseInt(width));
 }
