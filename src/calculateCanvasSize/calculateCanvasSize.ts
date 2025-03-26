9b8a5f62d36784008fca8bc5a09d76a30d4c2b87
Jian Gong <gongjian1981@gmail.com>
2025-03-26 15:13:50 -0400

    fix: edit code in calculateCanvasSize to pass the test for negative values

--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -2,5 +2,22 @@ export default function calculateCanvasSize(
   length: string,
   width: string
 ) {
-  return parseInt(length) * parseInt(width);
+  let l = parseInt(length);
+  if (Number.isNaN(l)) {
+    return NaN;
+  }
+  let w = parseInt(width);
+  if (Number.isNaN(w)) {
+    return NaN;
+  }
+  if (l == 0 || w == 0) {
+    return 0;
+  }
+  if (l < 0) {
+    l = -l;
+  }
+  if (w < 0) {
+    w = -w;
+  }
+  return l * w;
 }
