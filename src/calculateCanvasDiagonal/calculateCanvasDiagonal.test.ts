70583b5b29a14024058a518246bdcdf26bed53c5
Jian Gong <gongjian1981@gmail.com>
2025-03-26 15:06:05 -0400

    add: add test for calculateCanvasDiagonal for one 0 parameter

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.test.ts
@@ -8,6 +8,8 @@ describe('calculateCanvasDiagonal', () => {
   });
 
   test('should return 0 for length and width of 0', () => {
+    expect(calculateCanvasDiagonal('0', '1')).toBe(0);
+    expect(calculateCanvasDiagonal('1', '0')).toBe(0);
     expect(calculateCanvasDiagonal('0', '0')).toBe(0);
   });
 
