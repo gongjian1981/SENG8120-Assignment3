9ca0270f93a0e3263cd1e80babc4d89fe94edd6e
Jian Gong <gongjian1981@gmail.com>
2025-03-26 15:01:20 -0400

    fix: edit code in calculateCanvasPerimeter to pass the test for negative number

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
@@ -2,10 +2,16 @@ export default function calculateCanvasPerimeter(
   length: string,
   width: string
 ) {
-  const l = parseInt(length);
-  const w = parseInt(width);
+  let l = parseInt(length);
+  let w = parseInt(width);
   if (l == 0 || w == 0) {
     return 0;
   }
-  return 2 * (parseInt(length) + parseInt(width));
+  if (l < 0) {
+    l = -l;
+  }
+  if (w < 0) {
+    w = -w;
+  }
+  return 2 * (l + w);
 }
