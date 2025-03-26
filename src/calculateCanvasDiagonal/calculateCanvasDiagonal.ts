8855b5aed89e850c72657cecbf863c9b189f39f1
Jian Gong <gongjian1981@gmail.com>
2025-03-26 15:07:22 -0400

    fix: edit code in calculateCanvasDiagonal to pass the test for one zero parameter

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
@@ -3,6 +3,12 @@ export default function calculateCanvasDiagonal(
   width: string
 ) {
   const l = parseInt(length);
+  if (l == 0) {
+    return 0;
+  }
   const w = parseInt(width);
+  if (w == 0) {
+    return 0;
+  }
   return Math.pow(Math.pow(l, 2) + Math.pow(w, 2), 0.5);
 }
